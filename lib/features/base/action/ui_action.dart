linagora/tmail-flutter

TF-3157 Implement web socket push

lib/features/base/action/ui_action.dart
@@ -7,11 +7,9 @@ abstract class Action with EquatableMixin {}
 
 abstract class UIAction extends Action {}
 
-abstract class FcmAction extends Action {}
-
-abstract class FcmStateChangeAction extends FcmAction {
+abstract class PushNotificationStateChangeAction extends UIAction {
   final TypeName typeName;
   final jmap.State newState;
 
-  FcmStateChangeAction(this.typeName, this.newState);
+  PushNotificationStateChangeAction(this.typeName, this.newState);
 }

lib/features/push_notification/data/model/web_socket_ticket.dart
@@ -1,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'web_socket_ticket.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class WebSocketTicket with EquatableMixin {
+  final String? ticket;
+  final String? clientAddress;
+  final DateTime? generatedOn;
+  final DateTime? validUntil;
+
+  WebSocketTicket({
+    required this.ticket,
+    required this.clientAddress,
+    required this.generatedOn,
+    required this.validUntil,
+  });
+
+  factory WebSocketTicket.fromJson(Map<String, dynamic> json) => _$WebSocketTicketFromJson(json);
+  Map<String, dynamic> toJson() => _$WebSocketTicketToJson(this);
+
+  @override
+  List<Object?> get props => [
+    ticket,
+    clientAddress,
+    generatedOn,
+    validUntil];
+}

lib/features/push_notification/presentation/controller/fcm_token_controller.dart
@@ -20,12 +20,12 @@ import 'package:tmail_ui_user/features/push_notification/domain/usecases/get_sto
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/register_new_firebase_registration_token_interactor.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/store_firebase_registration_interator.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/update_firebase_registration_token_interactor.dart';
-import 'package:tmail_ui_user/features/push_notification/presentation/controller/fcm_base_controller.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/controller/push_notification_base_controller.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/utils/fcm_utils.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 import 'package:uuid/uuid.dart';
 
-class FcmTokenController extends FcmBaseController {
+class FcmTokenController extends PushNotificationBaseController {
 
   FcmTokenController._internal();
 

lib/features/push_notification/presentation/controller/web_socket_controller.dart
@@ -1,0 +1,62 @@
+import 'dart:async';
+
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/web_socket_push_state.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/usecases/connect_web_socket_interactor.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/controller/push_notification_base_controller.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/extensions/state_change_extension.dart';
+import 'package:tmail_ui_user/main/routes/route_navigation.dart';
+
+class WebSocketController extends PushNotificationBaseController {
+  WebSocketController._internal();
+
+  static final WebSocketController _instance = WebSocketController._internal();
+
+  static WebSocketController get instance => _instance;
+
+  ConnectWebSocketInteractor? _connectWebSocketInteractor;
+
+  @override
+  void handleFailureViewState(Failure failure) {
+    logError('WebSocketController::handleFailureViewState():Failure $failure');
+  }
+
+  @override
+  void handleSuccessViewState(Success success) {
+    log('WebSocketController::handleSuccessViewState():Success $success');
+    if (success is WebSocketPushStateReceived) {
+      _handleWebSocketPushStateReceived(success);
+    }
+  }
+
+  @override
+  void initialize({AccountId? accountId, Session? session}) {
+    super.initialize(accountId: accountId, session: session);
+
+    _connectWebSocket(accountId, session);
+  }
+
+  Future<void> _connectWebSocket(AccountId? accountId, Session? session) async {
+    _connectWebSocketInteractor = getBinding<ConnectWebSocketInteractor>();
+    if (_connectWebSocketInteractor == null || accountId == null || session == null) {
+      return;
+    }
+
+    consumeState(_connectWebSocketInteractor!.execute(session, accountId));
+  }
+  
+  void _handleWebSocketPushStateReceived(WebSocketPushStateReceived success) {
+    if (accountId == null || session == null) return;
+    final stateChange = success.stateChange;
+    final mapTypeState = stateChange.getMapTypeState(accountId!);
+    mappingTypeStateToAction(
+      mapTypeState,
+      accountId!,
+      session!.username,
+      session: session);
+  }
+}

donateko/scopos-webmail

lib/features/base/action/ui_action.dart
@@ -7,11 +7,9 @@ abstract class Action with EquatableMixin {}
 
 abstract class UIAction extends Action {}
 
-abstract class FcmAction extends Action {}
-
-abstract class FcmStateChangeAction extends FcmAction {
+abstract class PushNotificationStateChangeAction extends Action {
   final TypeName typeName;
   final jmap.State newState;
 
-  FcmStateChangeAction(this.typeName, this.newState);
+  PushNotificationStateChangeAction(this.typeName, this.newState);
 }

lib/features/push_notification/data/model/web_socket_ticket.dart
@@ -1,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'web_socket_ticket.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class WebSocketTicket with EquatableMixin {
+  final String? value;
+  final String? clientAddress;
+  final DateTime? generatedOn;
+  final DateTime? validUntil;
+  final String? username;
+
+  WebSocketTicket({
+    required this.value,
+    required this.clientAddress,
+    required this.generatedOn,
+    required this.validUntil,
+    required this.username,
+  });
+
+  factory WebSocketTicket.fromJson(Map<String, dynamic> json) => _$WebSocketTicketFromJson(json);
+  Map<String, dynamic> toJson() => _$WebSocketTicketToJson(this);
+
+  @override
+  List<Object?> get props => [
+    value,
+    clientAddress,
+    generatedOn,
+    validUntil,
+    username,
+  ];
+}

lib/features/push_notification/presentation/controller/fcm_token_controller.dart
@@ -20,12 +20,12 @@ import 'package:tmail_ui_user/features/push_notification/domain/usecases/get_sto
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/register_new_firebase_registration_token_interactor.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/store_firebase_registration_interator.dart';
 import 'package:tmail_ui_user/features/push_notification/domain/usecases/update_firebase_registration_token_interactor.dart';
-import 'package:tmail_ui_user/features/push_notification/presentation/controller/fcm_base_controller.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/controller/push_base_controller.dart';
 import 'package:tmail_ui_user/features/push_notification/presentation/utils/fcm_utils.dart';
 import 'package:tmail_ui_user/main/routes/route_navigation.dart';
 import 'package:uuid/uuid.dart';
 
-class FcmTokenController extends FcmBaseController {
+class FcmTokenController extends PushBaseController {
 
   FcmTokenController._internal();
 

lib/features/push_notification/presentation/controller/web_socket_controller.dart
@@ -1,0 +1,65 @@
+import 'package:core/presentation/state/failure.dart';
+import 'package:core/presentation/state/success.dart';
+import 'package:core/utils/app_logger.dart';
+import 'package:jmap_dart_client/jmap/account_id.dart';
+import 'package:jmap_dart_client/jmap/core/session/session.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/state/web_socket_push_state.dart';
+import 'package:tmail_ui_user/features/push_notification/domain/usecases/connect_web_socket_interactor.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/controller/push_base_controller.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/extensions/state_change_extension.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/listener/email_change_listener.dart';
+import 'package:tmail_ui_user/features/push_notification/presentation/listener/mailbox_change_listener.dart';
+import 'package:tmail_ui_user/main/routes/route_navigation.dart';
+
+class WebSocketController extends PushBaseController {
+  WebSocketController._internal();
+
+  static final WebSocketController _instance = WebSocketController._internal();
+
+  static WebSocketController get instance => _instance;
+
+  ConnectWebSocketInteractor? _connectWebSocketInteractor;
+
+  @override
+  void handleFailureViewState(Failure failure) {
+    logError('WebSocketController::handleFailureViewState():Failure $failure');
+  }
+
+  @override
+  void handleSuccessViewState(Success success) {
+    log('WebSocketController::handleSuccessViewState():Success $success');
+    if (success is WebSocketPushStateReceived) {
+      _handleWebSocketPushStateReceived(success);
+    }
+  }
+
+  @override
+  void initialize({AccountId? accountId, Session? session}) {
+    super.initialize(accountId: accountId, session: session);
+
+    _connectWebSocket(accountId, session);
+  }
+
+  void _connectWebSocket(AccountId? accountId, Session? session) {
+    _connectWebSocketInteractor = getBinding<ConnectWebSocketInteractor>();
+    if (_connectWebSocketInteractor == null || accountId == null || session == null) {
+      return;
+    }
+
+    consumeState(_connectWebSocketInteractor!.execute(session, accountId));
+  }
+  
+  void _handleWebSocketPushStateReceived(WebSocketPushStateReceived success) {
+    log('WebSocketController::_handleWebSocketPushStateReceived(): $success');
+    if (accountId == null || session == null) return;
+    final stateChange = success.stateChange;
+    final mapTypeState = stateChange.getMapTypeState(accountId!);
+    mappingTypeStateToAction(
+      mapTypeState,
+      accountId!,
+      emailChangeListener: EmailChangeListener.instance,
+      mailboxChangeListener: MailboxChangeListener.instance,
+      session!.username,
+      session: session);
+  }
+}