aryan-more/tmail-flutter

TF-3236 Widget test RichTextBuilder

core/lib/presentation/extensions/list_extensions.dart
@@ -16,4 +16,8 @@ extension ListExtensions<T> on List<T> {
       validBuilder.build().toList(),
       validBuilder.build().toList());
   }
+
+  int countOccurences(T value) {
+    return where((element) => element == value).length;
+  }
 }

donateko/scopos-webmail

core/lib/presentation/extensions/list_extensions.dart
@@ -16,4 +16,8 @@ extension ListExtensions<T> on List<T> {
       validBuilder.build().toList(),
       validBuilder.build().toList());
   }
+
+  int countOccurrences(T value) {
+    return where((element) => element == value).length;
+  }
 }