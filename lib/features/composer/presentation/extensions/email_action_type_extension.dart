linagora/tmail-flutter

TF-3759 Update style popup context menu for mailbox view
Signed-off-by: dab246 <tdvu@linagora.com>

lib/features/composer/presentation/extensions/email_action_type_extension.dart
@@ -1,7 +1,9 @@
 
+import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/extensions/html_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:model/email/email_action_type.dart';
 import 'package:model/email/presentation_email.dart';
 import 'package:model/extensions/list_email_address_extension.dart';
@@ -179,6 +181,24 @@ extension EmailActionTypeExtension on EmailActionType {
         return appLocalizations.openInNewTab;
       default:
         return '';
+    }
+  }
+
+  Color getContextMenuIconColor() {
+    switch(this) {
+      case EmailActionType.deletePermanently:
+        return AppColor.redFF3347;
+      default:
+        return AppColor.steelGrayA540;
+    }
+  }
+
+  Color getContextMenuTitleColor() {
+    switch(this) {
+      case EmailActionType.deletePermanently:
+        return AppColor.redFF3347;
+      default:
+        return Colors.black;
     }
   }
 }

lib/features/email/presentation/email_view.dart
@@ -555,7 +555,7 @@ class EmailView extends GetWidget<SingleEmailController> {
         _emailActionMoreActionTile(context, presentationEmail, moreActions)
       );
     } else {
-      final popupMenuActions = moreActions.map((actionType) {
+      final popupMenuItems = moreActions.map((actionType) {
         return PopupMenuItem(
           padding: EdgeInsets.zero,
           child: PopupMenuItemActionWidget(
@@ -576,11 +576,7 @@ class EmailView extends GetWidget<SingleEmailController> {
         );
       }).toList();
 
-      controller.openPopupMenuAction(
-        context,
-        position,
-        popupMenuActions,
-      );
+      controller.openPopupMenuAction(context, position, popupMenuItems);
     }
   }
 

donateko/scopos-webmail

lib/features/composer/presentation/extensions/email_action_type_extension.dart
@@ -1,7 +1,8 @@
 
+import 'package:core/presentation/extensions/color_extension.dart';
 import 'package:core/presentation/extensions/html_extension.dart';
 import 'package:core/presentation/resources/image_paths.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:model/email/email_action_type.dart';
 import 'package:model/email/presentation_email.dart';
 import 'package:model/extensions/list_email_address_extension.dart';
@@ -179,6 +180,24 @@ extension EmailActionTypeExtension on EmailActionType {
         return appLocalizations.openInNewTab;
       default:
         return '';
+    }
+  }
+
+  Color getPopupMenuIconColor() {
+    switch(this) {
+      case EmailActionType.deletePermanently:
+        return AppColor.redFF3347;
+      default:
+        return AppColor.steelGrayA540;
+    }
+  }
+
+  Color getPopupMenuTitleColor() {
+    switch(this) {
+      case EmailActionType.deletePermanently:
+        return AppColor.redFF3347;
+      default:
+        return Colors.black;
     }
   }
 }

lib/features/email/presentation/email_view.dart
@@ -574,7 +574,7 @@ class EmailView extends GetWidget<SingleEmailController> {
         },
       );
     } else {
-      final popupMenuActions = emailActions.map((actionType) {
+      final popupMenuItems = emailActions.map((actionType) {
         return PopupMenuItem(
           padding: EdgeInsets.zero,
           child: PopupMenuItemActionWidget(
@@ -595,11 +595,7 @@ class EmailView extends GetWidget<SingleEmailController> {
         );
       }).toList();
 
-      controller.openPopupMenuAction(
-        context,
-        position,
-        popupMenuActions,
-      );
+      controller.openPopupMenuAction(context, position, popupMenuItems);
     }
   }
 }