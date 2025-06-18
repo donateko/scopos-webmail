zacares/tmail-flutter

TF-3712 Add SettingsCapability to hide language setting

lib/features/manage_account/presentation/manage_account_dashboard_view.dart
@@ -103,7 +103,11 @@ class ManageAccountDashBoardView extends GetWidget<ManageAccountDashBoardControl
         case AccountMenuItem.profiles:
           return ProfilesView(responsiveUtils: controller.responsiveUtils);
         case AccountMenuItem.languageAndRegion:
-          return const LanguageAndRegionView();
+          if (controller.isLanguageSupported) {
+            return const LanguageAndRegionView();
+          } else {
+            return const SizedBox.shrink();
+          }
         case AccountMenuItem.emailRules:
           if (controller.isRuleFilterCapabilitySupported){
             return EmailRulesView();

lib/features/manage_account/presentation/menu/manage_account_menu_controller.dart
@@ -46,7 +46,8 @@ class ManageAccountMenuController extends GetxController with ContactSupportMixi
       if (dashBoardController.isVacationCapabilitySupported)
         AccountMenuItem.vacation,
       AccountMenuItem.mailboxVisibility,
-      AccountMenuItem.languageAndRegion
+      if (dashBoardController.isLanguageSupported)
+        AccountMenuItem.languageAndRegion,
     ];
     listAccountMenuItem.value = newListMenuSetting;
 

lib/features/manage_account/presentation/menu/settings/settings_first_level_view.dart
@@ -128,17 +128,25 @@ class SettingsFirstLevelView extends GetWidget<SettingsController> {
           subtitle: AppLocalizations.of(context).folderVisibilitySubtitle,
           () => controller.selectSettings(AccountMenuItem.mailboxVisibility)
         ),
-        Divider(
-          color: AppColor.colorDividerHorizontal,
-          height: 1,
-          indent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils),
-          endIndent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils)
-        ),
-        SettingFirstLevelTileBuilder(
-          AccountMenuItem.languageAndRegion.getName(AppLocalizations.of(context)),
-          AccountMenuItem.languageAndRegion.getIcon(controller.imagePaths),
-          () => controller.selectSettings(AccountMenuItem.languageAndRegion)
-        ),
+        Obx(() {
+          if (!controller.manageAccountDashboardController.isLanguageSupported) {
+            return const SizedBox.shrink();
+          }
+
+          return Column(children: [
+            Divider(
+              color: AppColor.colorDividerHorizontal,
+              height: 1,
+              indent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils),
+              endIndent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils)
+            ),
+            SettingFirstLevelTileBuilder(
+              AccountMenuItem.languageAndRegion.getName(AppLocalizations.of(context)),
+              AccountMenuItem.languageAndRegion.getIcon(controller.imagePaths),
+              () => controller.selectSettings(AccountMenuItem.languageAndRegion)
+            ),
+          ]);
+        }),
         Obx(() {
           if (controller.manageAccountDashboardController.isFcmCapabilitySupported && PlatformInfo.isMobile) {
             return Column(children: [

lib/features/manage_account/presentation/menu/settings/settings_view.dart
@@ -197,7 +197,11 @@ class SettingsView extends GetWidget<SettingsController> {
         case AccountMenuItem.profiles:
           return ProfilesView(responsiveUtils: controller.responsiveUtils);
         case AccountMenuItem.languageAndRegion:
-          return const LanguageAndRegionView();
+          if (controller.manageAccountDashboardController.isLanguageSupported) {
+            return const LanguageAndRegionView();
+          } else {
+            return const SizedBox.shrink();
+          }
         case AccountMenuItem.emailRules:
           if (controller.manageAccountDashboardController.isRuleFilterCapabilitySupported) {
             return EmailRulesView();

donateko/scopos-webmail

lib/features/manage_account/presentation/manage_account_dashboard_view.dart
@@ -119,7 +119,11 @@ class ManageAccountDashBoardView extends GetWidget<ManageAccountDashBoardControl
         case AccountMenuItem.profiles:
           return ProfilesView(responsiveUtils: controller.responsiveUtils);
         case AccountMenuItem.languageAndRegion:
-          return const LanguageAndRegionView();
+          if (controller.isLanguageSettingDisplayed) {
+            return const LanguageAndRegionView();
+          } else {
+            return const SizedBox.shrink();
+          }
         case AccountMenuItem.emailRules:
           if (controller.isRuleFilterCapabilitySupported){
             return EmailRulesView();

lib/features/manage_account/presentation/menu/manage_account_menu_controller.dart
@@ -46,7 +46,8 @@ class ManageAccountMenuController extends GetxController with ContactSupportMixi
       if (dashBoardController.isVacationCapabilitySupported)
         AccountMenuItem.vacation,
       AccountMenuItem.mailboxVisibility,
-      AccountMenuItem.languageAndRegion
+      if (dashBoardController.isLanguageSettingDisplayed)
+        AccountMenuItem.languageAndRegion,
     ];
     listAccountMenuItem.value = newListMenuSetting;
 

lib/features/manage_account/presentation/menu/settings/settings_first_level_view.dart
@@ -128,17 +128,25 @@ class SettingsFirstLevelView extends GetWidget<SettingsController> {
           subtitle: AppLocalizations.of(context).folderVisibilitySubtitle,
           () => controller.selectSettings(AccountMenuItem.mailboxVisibility)
         ),
-        Divider(
-          color: AppColor.colorDividerHorizontal,
-          height: 1,
-          indent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils),
-          endIndent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils)
-        ),
-        SettingFirstLevelTileBuilder(
-          AccountMenuItem.languageAndRegion.getName(AppLocalizations.of(context)),
-          AccountMenuItem.languageAndRegion.getIcon(controller.imagePaths),
-          () => controller.selectSettings(AccountMenuItem.languageAndRegion)
-        ),
+        Obx(() {
+          if (!controller.manageAccountDashboardController.isLanguageSettingDisplayed) {
+            return const SizedBox.shrink();
+          }
+
+          return Column(children: [
+            Divider(
+              color: AppColor.colorDividerHorizontal,
+              height: 1,
+              indent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils),
+              endIndent: SettingsUtils.getHorizontalPadding(context, controller.responsiveUtils)
+            ),
+            SettingFirstLevelTileBuilder(
+              AccountMenuItem.languageAndRegion.getName(AppLocalizations.of(context)),
+              AccountMenuItem.languageAndRegion.getIcon(controller.imagePaths),
+              () => controller.selectSettings(AccountMenuItem.languageAndRegion)
+            ),
+          ]);
+        }),
         Obx(() {
           if (controller.manageAccountDashboardController.isFcmCapabilitySupported && PlatformInfo.isMobile) {
             return Column(children: [

lib/features/manage_account/presentation/menu/settings/settings_view.dart
@@ -197,7 +197,11 @@ class SettingsView extends GetWidget<SettingsController> {
         case AccountMenuItem.profiles:
           return ProfilesView(responsiveUtils: controller.responsiveUtils);
         case AccountMenuItem.languageAndRegion:
-          return const LanguageAndRegionView();
+          if (controller.manageAccountDashboardController.isLanguageSettingDisplayed) {
+            return const LanguageAndRegionView();
+          } else {
+            return const SizedBox.shrink();
+          }
         case AccountMenuItem.emailRules:
           if (controller.manageAccountDashboardController.isRuleFilterCapabilitySupported) {
             return EmailRulesView();